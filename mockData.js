7be73b0a2af7a3d6f3ceb0242084842edc8d70e6
María José Fariña <149617571+mariajosef19@users.noreply.github.com>
2025-12-15 21:38:26 -0300

    Add mock data for tareas

---
+++ mockData.js
@@ -0,0 +1,63 @@
+const tareasMock = [
+    {
+        id: 1,
+        nombre: "Clase de Programación",
+        fecha: "2025-12-11",
+        prioridad: "alta"
+    },
+    {
+        id: 2,
+        nombre: "Hacer limpieza general en casa",
+        fecha: "2025-12-12",
+        prioridad: "media"
+        
+    },
+    {
+        id: 3,
+        nombre: "Hacer la comida para Navidad",
+        fecha: "2025-12-23",
+        prioridad: "media"
+    },
+    {
+        id: 4,
+        nombre: "Organizar archivos del curso",
+        fecha: "2025-12-14",
+        prioridad: "baja"
+    },
+    {
+        id: 5,
+        nombre: "Corregir errores del trabajo Grupal",
+        fecha: "2025-12-15",
+        prioridad: "alta"
+    },
+    {
+        id: 6,
+        nombre: "Cocinar algo saludable para la semana",
+        fecha: "2025-12-15",
+        prioridad: "baja"
+    },
+    {
+        id: 7,
+        nombre: "Exposicion del trabajo Grupal",
+        fecha: "2025-12-16",
+        prioridad: "alta"
+    },
+    {
+        id: 8,
+        nombre: "Ultima Clase Social del Curso MediaPila",
+        fecha: "2025-12-17",
+        prioridad: "alta"
+    },
+    {
+        id: 9,
+        nombre: "Leer un capítulo de un libro",
+        fecha: "2025-12-19",
+        prioridad: "baja"
+    },
+    {
+        id: 10,
+        nombre: "Juntarse con las chicas del Grupo 6",
+        fecha: "2025-12-22",
+        prioridad: "alta"
+    }
+];
